25cbe0cbbff22846f624dc19b5aa0830a9453481
Pratyush Kar <pratyush.kar@gmail.com>
2018-11-17 21:45:57 -0600

    added basic code for GeometricPathPlanning

--- core/python/path.py
+++ core/python/path.py
@@ -1,3 +1,7 @@
+import pdb
+import math
+import heapq
+
 class Point2D:
 	def __init__(self, x, y):
 		self.x = x
@@ -5,6 +9,9 @@ class Point2D:
 
 	def loc(self):
 		return (self.x, self.y)
+
+	def dist(self, pt):
+		return math.sqrt((self.x - pt.x) ** 2 + (self.y - pt.y) ** 2)
 
 	def __str__(self):
 		return '(' + str(self.x) + ', ' + str(self.y) + ')'
@@ -22,8 +29,50 @@ class Pose2D:
 
 class Obstacle:
 	def __init__(self, x, y, r):
-		self.pos = Point2D(x, y)
-		self.r = r
+		self.pos = Point2D(float(x), float(y))
+		self.r = float(r)
+
+	def _is_overlapping(self, obs):
+		dist = math.sqrt((self.pos.x - obs.pos.x) ** 2 + (self.pos.y - obs.pos.y) ** 2)
+		return dist < self.r + obs.r
+
+	def _is_inside(self, pt):
+		dist = math.sqrt((self.pos.x - pt.x) ** 2 + (self.pos.y - pt.y) ** 2)
+		return dist < self.r
+
+	def _merge_together(self, obs):
+		dist = math.sqrt((self.pos.x - obs.pos.x) ** 2 + (self.pos.y - obs.pos.y) ** 2)
+		self.pos = Point2D((self.pos.x + obs.pos.x) / 2.0, (self.pos.y + obs.pos.y) / 2.0)
+		self.r = max(self.r, obs.r) + dist / 2.0
+
+class Line2D:
+	def __init__(self, p1, p2):
+		self.pt1 = p1
+		self.pt2 = p2
+
+	def _intersect_with_circle(self, obs):
+		a = (self.pt2.x - self.pt1.x) ** 2 + (self.pt2.y - self.pt1.y) ** 2
+		b = 2. * ((self.pt1.x - obs.pos.x) * (self.pt2.x - self.pt1.x) + \
+			(self.pt1.y - obs.pos.y) * (self.pt2.y - self.pt1.y))
+		c = (self.pt1.x - obs.pos.x) ** 2 + (self.pt1.y - obs.pos.y) ** 2 - (obs.r ** 2)
+
+		discrim = b * b - 4. * a * c
+
+		if discrim < 0:
+			return False
+
+		else:
+			discrim = math.sqrt(discrim)
+
+			t1 = (-b - discrim) / (2. * a)
+			t2 = (-b + discrim) / (2. * a)
+
+			if t1 >= 0 and t1 <= 1:
+				return True
+			elif t2 >= 0 and t2 <= 1:
+				return True
+
+			return False
 
 class PathPlanner:
 	def __init__(self):
@@ -32,3 +81,135 @@ class PathPlanner:
 
 	def update(self):
 		pass
+
+class GeometricPathPlanner(PathPlanner):
+
+	class VisibilityGraph:
+		class Edge:
+			def __init__(self, i1, i2, pt1, pt2, cost, etype):
+				self.i1 = i1
+				self.i2 = i2
+				self.pt1 = pt1
+				self.pt2 = pt2
+				self.etype = etype
+				self.cost = cost
+		def __init__(self):
+			self._nvertex = 2
+			self._graph = {}
+			self.ARC_COST_MULTIPLIER = 1.2
+
+		def add_edge(self, i1, i2, pt1, pt2, cost, etype):
+			if i1 not in self._graph:
+				self._graph[i1] = []
+			if i2 not in self._graph:
+				self._graph[i2] = []
+			self._graph[i1].append(self.Edge(i1, i2, pt1, pt2, cost, etype))
+			self._graph[i2].append(self.Edge(i2, i1, pt2, pt1, cost, etype))
+
+		def dijkstras(self):
+			min_dist = [float('inf') if i != 0 else 0.0 for i in range(self._nvertex)]
+			visited = [False for i in range(self._nvertex)]
+			back_pointers = [None for i in range(self._nvertex)]
+			pr_queue = []
+			heapq.heappush(pr_queue, (0., 0))
+
+			while len(pr_queue) > 0:
+				cost, idx = heapq.heappop(pr_queue)
+
+				if visited[idx]:
+					continue
+				visited[idx] = True
+
+				for edge in self._graph[idx]:
+					ecost = edge.cost * (1.0 if edge.etype == 'line' else self.ARC_COST_MULTIPLIER)
+					nidx = edge.i2
+					if min_dist[nidx] > cost + ecost:
+						min_dist[nidx] = cost + ecost
+						back_pointers[nidx] = (idx, (edge.pt1, edge.pt2, edge.etype))
+						heapq.heappush(pr_queue, (cost + ecost, nidx))
+
+			path = []
+			idx = 1
+			while idx != 0:
+				path.append(back_pointers[idx][1])
+				idx = back_pointers[idx][0]
+
+			path.reverse()
+			return path
+
+	def __init__(self):
+		super(GeometricPathPlanner, self).__init__()
+		self._obstacles = []
+		self._target = None
+		self._vis_graph = self.VisibilityGraph()
+		self._path = None
+
+	def _get_tangent_pts(self, obs, pt):
+		dx = obs.pos.x - pt.x
+		dy = obs.pos.y - pt.y
+		dd = math.sqrt(dx ** 2 + dy ** 2)
+		r = obs.r
+
+		if r / dd < 1.0:
+			a = math.asin(r / dd)
+		else:
+			raise ValueError('Cannot calculate tangent from a point within the circle')
+		b = math.atan2(dy, dx)
+
+		t = b - a
+		ta = Point2D(obs.pos.x + r * math.sin(t), obs.pos.y + r * -math.cos(t))
+		t = b + a
+		tb = Point2D(obs.pos.x + r * -math.sin(t), obs.pos.y + r * math.cos(t))
+
+		return ta, tb
+
+	def _get_arc_length(self, pt1, pt2, r):
+		chord_len = pt1.dist(pt2)
+		angle = 2. * math.asin(chord_len / (2. * r))
+		return angle * r
+
+	def _update_visibility_graph(self, src, tgt, i1=0, i2=1, ignore_obs=[]):
+		print(i1, i2, ignore_obs)
+		mline = Line2D(src, tgt)
+		intersecting_obs = []
+		for i, obs in enumerate(self._obstacles):
+			if i not in ignore_obs and mline._intersect_with_circle(obs):
+				intersecting_obs.append((i, obs))
+
+		if len(intersecting_obs) == 0:	# straight line path available
+			cost = src.dist(tgt)
+			# updating the vis graph
+			self._vis_graph.add_edge(i1, i2, src, tgt, cost, 'line')
+		else:
+			intersecting_obs.sort(key=lambda obs_tup: src.dist(obs_tup[1].pos))
+			o1, o2 = self._get_tangent_pts(intersecting_obs[0][1], src)
+			o3, o4 = self._get_tangent_pts(intersecting_obs[0][1], tgt)
+			r = intersecting_obs[0][1].r
+			if self._get_arc_length(o1, o3, r) > self._get_arc_length(o1, o4, r):
+				o4, o3 = o3, o4
+			io1 = self._vis_graph._nvertex
+			io2 = self._vis_graph._nvertex + 1
+			io3 = self._vis_graph._nvertex + 2
+			io4 = self._vis_graph._nvertex + 3
+			self._vis_graph._nvertex += 4
+
+			# updating the vis graph
+			self._vis_graph.add_edge(io1, io3, o1, o3, self._get_arc_length(o1, o3, r), 'arc')
+			self._vis_graph.add_edge(io2, io4, o2, o4, self._get_arc_length(o2, o4, r), 'arc')
+			self._update_visibility_graph(src, o1, i1, io1, ignore_obs=ignore_obs + [intersecting_obs[0][0]])
+			self._update_visibility_graph(src, o2, i1, io2, ignore_obs=ignore_obs + [intersecting_obs[0][0]])
+			self._update_visibility_graph(o3, tgt, io3, i2, ignore_obs=ignore_obs + [intersecting_obs[0][0]])
+			self._update_visibility_graph(o4, tgt, io4, i2, ignore_obs=ignore_obs + [intersecting_obs[0][0]])
+
+	def update(self, obstacles, target):
+		self._obstacles = obstacles
+		self._target = target
+		self._vis_graph = self.VisibilityGraph()
+		try:
+			self._update_visibility_graph(Point2D(0, 0), self._target)
+			self._path = self._vis_graph.dijkstras()
+		except:
+			print ('Warning: Cannot calculate tangent from a point within the circle')
+
+		return self._path
+
--- core/python/test_path.py
+++ core/python/test_path.py
@@ -11,19 +11,20 @@ class PathTester:
 		self.max_x = max_x
 		self.max_y = max_y
 		self.border_size = border_size
-		self.num_obstacles = num_obstacles
 
 		self._robot_loc = Pose2D(self._get_random_pos(), randu(0, 2 * math.pi))
-		self._obstacles = [self._get_random_pos() for i in range(num_obstacles)]
-		self._target = self._get_random_pos()
 		self._obstacle_radius = 30
+		self._obstacles = [Obstacle(*self._get_random_pos().loc(), self._obstacle_radius) for i in range(num_obstacles)]
+		self._target = self._get_random_pos()
+		self._validate_obstacles()
 
 		self.sim_img = None
 		self.reset_canvas()
 
-		self.C_ROBOT = (0, 0, 255)
-		self.C_OBSTACLE = (0, 255, 0)
-		self.C_TARGET = (255, 0, 0)
+		self.C_ROBOT = (0, 0, 255) # RED
+		self.C_OBSTACLE = (0, 255, 0) # GREEN
+		self.C_TARGET = (255, 0, 0) # BLUE
+		self.C_PATH = (255, 255, 255) # WHITE
 
 	def _get_random_pos(self):
 		return Point2D(randi(self.border_size, self.max_x - self.border_size), \
@@ -37,10 +38,19 @@ class PathTester:
 
 	def draw_obstacles(self):
 		for obs in self._obstacles:
-			cv2.circle(self.sim_img, obs.loc(), self._obstacle_radius, self.C_OBSTACLE, 1)
+			cv2.circle(self.sim_img, (int(obs.pos.x), int(obs.pos.y)), int(obs.r), self.C_OBSTACLE, 1)
 
 	def draw_target(self):
 		cv2.circle(self.sim_img, self._target.loc(), 5, self.C_TARGET, -1)
+
+	def draw_path(self, path):
+		for p in path:
+			if p[2] == 'line':
+				x1 = int(p[0].x + self._robot_loc.pos.x)
+				y1 = int(p[0].y + self._robot_loc.pos.y)
+				x2 = int(p[1].x + self._robot_loc.pos.x)
+				y2 = int(p[1].y + self._robot_loc.pos.y)
+				cv2.line(self.sim_img, (x1, y1), (x2, y2), self.C_PATH, 1)
 
 	def draw(self):
 		self.reset_canvas()
@@ -53,50 +63,87 @@ class PathTester:
 		cv2.imshow('dst_rt', img)
 		cv2.setMouseCallback('dst_rt', self.click_callback)
 
+	def _merge_obstacles(self):
+		m_obstacles = []
+		for obs in self._obstacles:
+			match = False
+			for cluster in m_obstacles:
+				if len([o for o in cluster if obs._is_overlapping(o)]) > 0:
+					cluster.append(obs)
+					match = True
+					break
+			if not match:
+				m_obstacles.append([obs])
+
+		self._obstacles = []
+		for cluster in m_obstacles:
+			obs = cluster[0]
+			for o in cluster[1:]:
+				obs._merge_together(o)
+			self._obstacles.append(obs)
+
+	def _validate_obstacles(self):
+		self._merge_obstacles()
+		new_obstacles = [obs for obs in self._obstacles if not (obs._is_inside(self._robot_loc.pos) or obs._is_inside(self._target))]
+		self._obstacles = new_obstacles
+
 	def click_callback(self, event, x, y, flags, param):
-		
-		def inside_obstacle(x, y, obs):
-			dist = math.sqrt((x - obs.x) ** 2 + (y - obs.y) ** 2)
-			if dist < self._obstacle_radius:
-				return True
-			else:
-				return False
 
 		if event == cv2.EVENT_MBUTTONDOWN:
 			self._target = Point2D(x, y)
 
 		elif event == cv2.EVENT_LBUTTONDOWN:
-			new_obstacles = [obs for obs in self._obstacles if not inside_obstacle(x, y, obs)]
-			if len(new_obstacles) != self.num_obstacles:
-				self.num_obstacles = len(new_obstacles)
+			new_obstacles = [obs for obs in self._obstacles if not obs._is_inside(Point2D(x, y))]
+			if len(new_obstacles) != len(self._obstacles):
 				self._obstacles = new_obstacles
 			else:
-				self.num_obstacles += 1
-				self._obstacles.append(Point2D(x, y))
+				self._obstacles.append(Obstacle(x, y, self._obstacle_radius))
+
+			self._validate_obstacles()
 
 		else:
 			pass
+
+	def get_obstacles_egocentric(self):
+		self._validate_obstacles()
+		obstacles = [Obstacle(obs.pos.x - self._robot_loc.pos.x, obs.pos.y - self._robot_loc.pos.y, \
+			obs.r) for obs in self._obstacles]
+		return obstacles
+
+	def get_target_egocentric(self):
+		return Point2D(self._target.x - self._robot_loc.pos.x, self._target.y - self._robot_loc.pos.y)
+
+	def get_keypoints_egocentric(self):
+		return self.get_obstacles_egocentric(), self.get_target_egocentric()
 
 	def __str__(self):
 		ret = ''
 		ret += 'RPos: ' + self._robot_loc.__str__() + '\n'
 		ret += 'Obstacles:\n'
-		ret += '\n'.join([obs.__str__() for obs in self._obstacles])
+		ret += '\n'.join([obs.pos.__str__() for obs in self._obstacles])
 		ret += '\n'
 		ret += 'Target: ' + self._target.__str__() + '\n'
 
 		return ret
 
 
-
 p = PathTester()
+p._robot_loc = Pose2D(Point2D(494, 357), 2.8743314216643725)
+p._obstacles = [Obstacle(288, 183, 30), Obstacle(311, 297, 30), Obstacle(430, 305, 30)]
+p._target = Point2D(196, 151)
+planner = GeometricPathPlanner()
 
 while True:
 	p.draw()
 	print(p)
+	path = planner.update(*p.get_keypoints_egocentric())
+	print([(pt1.__str__(), pt2.__str__(), etype) for pt1, pt2, etype in path])
+	p.draw_path(path)
 	p.show()
 	c = cv2.waitKey(10)
 
 	if c == ord('q') or c == 27:
 		break
+	elif c == ord('d'):
+		pdb.set_trace()
 
